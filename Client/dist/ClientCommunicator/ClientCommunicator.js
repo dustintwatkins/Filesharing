e8342b5f314d019581618efbd9441cf3c710a3c1
dustintwatkins <dustintwatkins@gmail.com>
2018-08-18 17:08:42 -0600

    Removed dist directories

--- Client/dist/ClientCommunicator/ClientCommunicator.js
+++
@@ -1,8 +0,0 @@
-var ClientCommunicator = /** @class */ (function () {
-    function ClientCommunicator() {
-    }
-    ClientCommunicator.server_url = 'http://localhost:8082';
-    return ClientCommunicator;
-}());
-export default ClientCommunicator;
-//# sourceMappingURL=ClientCommunicator.js.map
--- Client/dist/index.js
+++
@@ -1,5 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import App from './Components/App';
-ReactDOM.render(React.createElement(App, null), document.getElementById('app'));
-//# sourceMappingURL=index.js.map
